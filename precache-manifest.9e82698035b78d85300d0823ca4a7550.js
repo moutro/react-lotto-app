69e81ca697d8d390d53bf9a7f68255aa00354e6b
Jason B <philotimo@gmail.com>
2019-08-25 23:35:40 -0400

    Updates

---
+++ precache-manifest.9e82698035b78d85300d0823ca4a7550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abd234314d4a80d6c73b891525fa502",
+    "url": "/react-lotto-app/index.html"
+  },
+  {
+    "revision": "27b36208c8780f17ef6c",
+    "url": "/react-lotto-app/static/css/main.d2059087.chunk.css"
+  },
+  {
+    "revision": "dde97df702fc786c4af6",
+    "url": "/react-lotto-app/static/js/2.0da088c4.chunk.js"
+  },
+  {
+    "revision": "27b36208c8780f17ef6c",
+    "url": "/react-lotto-app/static/js/main.b4b91ab9.chunk.js"
+  },
+  {
+    "revision": "303a8f8c23fb0b9d985d",
+    "url": "/react-lotto-app/static/js/runtime~main.66c24995.js"
+  }
+]);
